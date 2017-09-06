b8f2a3ed6d36c858c849e5e84ec79b475a814858
GyverJeong <skjeong@emastec.com>
2017-09-06 13:22:43 +0900

    Modify indentation and path of NFS

--- build.sh
+++ build.sh
@@ -6,32 +6,32 @@ rm -f model_name.txt model_num.txt ver.txt *.bin
 sh script/genrule.sh $1 &&
 
 # build all
-make && 
+make &&
 
 # make image
 make Reinstall &&
 
 if [ -f model_num.txt ]
 then
-	N=`cat model_num.txt`		# model number
-	M=`cat model_name.txt`		# model name
-	v=`cat vendor.txt`			# vendor
-	V=`cat ver.txt`				# version
-	D=`date '+%Y%m%d'`			# date
+    N=`cat model_num.txt`       # model number
+    M=`cat model_name.txt`      # model name
+    v=`cat vendor.txt`          # vendor
+    V=`cat ver.txt`             # version
+    D=`date '+%Y%m%d'`          # date
 
-	BIN="cardvr_${M}_${v}_${V}_${D}.bin"
-	bin=`echo ${BIN} | tr A-Z a-z`
+    BIN="cardvr_${M}_${v}_${V}_${D}.bin"
+    bin=`echo ${BIN} | tr A-Z a-z`
 
-	echo ""
-	echo "make ${bin} for \"${M}_${v}\""
-	echo ""
+    echo ""
+    echo "make ${bin} for \"${M}_${v}\""
+    echo ""
 
-	sh script/makeCarDvr.sh ${N}
+    sh script/makeCarDvr.sh ${N}
 
-	cp cardvr.bin ${bin}
+    cp cardvr.bin ${bin}
 else
-	echo "No such model_num.txt"
-	exit 1
+    echo "No such model_num.txt"
+    exit 1
 fi
 
 exit 0
