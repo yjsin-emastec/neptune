02db75da2f24531ffee078f45168feed5fad197d
GyverJeong <skjeong@emastec.com>
2017-10-13 10:28:07 +0900

    Delete a tab space in GUI/languagedialog.cpp

--- gui/mainwidget/src/dvrsetup/system/language/languagedialog.cpp
+++ gui/mainwidget/src/dvrsetup/system/language/languagedialog.cpp
@@ -8,24 +8,24 @@
 LanguageDialog::LanguageDialog(QWidget *parent)
     : QDialog(parent)
 {
-	setupUi(this);
+    setupUi(this);
 
-	setWindowFlags(Qt::Dialog | Qt::FramelessWindowHint);
+    setWindowFlags(Qt::Dialog | Qt::FramelessWindowHint);
 
-	setPalette(QPalette(QColor(255, 128, 64)));
-	frame->setStyleSheet(".QFrame{background: rgb(39, 0, 79);}");
+    setPalette(QPalette(QColor(255, 128, 64)));
+    frame->setStyleSheet(".QFrame{background: rgb(39, 0, 79);}");
 
     msgBox = NULL;
-	buttonLanguageUp->setFocus();
+    buttonLanguageUp->setFocus();
 
-	lineEditLanguage->setStyleSheet("QLineEdit{font:64px;color:white;background-color:rgb(50,57,83);border-width:1px;border-style:inset;border-color:white;}");
-	lineEditLanguage->setAlignment(Qt::AlignCenter);
+    lineEditLanguage->setStyleSheet("QLineEdit{font:64px;color:white;background-color:rgb(50,57,83);border-width:1px;border-style:inset;border-color:white;}");
+    lineEditLanguage->setAlignment(Qt::AlignCenter);
 
-	buttonLanguageUp->setIcon(QIcon(":/images/arrow_up.png"));
-	buttonLanguageUp->setIconSize(QSize(90, 90));
+    buttonLanguageUp->setIcon(QIcon(":/images/arrow_up.png"));
+    buttonLanguageUp->setIconSize(QSize(90, 90));
 
-	buttonLanguageDown->setIcon(QIcon(":/images/arrow_down.png"));
-	buttonLanguageDown->setIconSize(QSize(90, 90));
+    buttonLanguageDown->setIcon(QIcon(":/images/arrow_down.png"));
+    buttonLanguageDown->setIconSize(QSize(90, 90));
 
     connect(buttonLanguageUp,   SIGNAL(released()), this, SLOT(onButtonLanguageUp()));
     connect(buttonLanguageDown, SIGNAL(released()), this, SLOT(onButtonLanguageDown()));
@@ -64,67 +64,67 @@ void LanguageDialog::onButtonLanguageSave()
 }
 void LanguageDialog::initLanguageConfig(void)
 {
-	buttonLanguageUp->setFocus();
+    buttonLanguageUp->setFocus();
 
-	if(utils_cfg_cmp_item(SystemCfg.language, "ENGLISH") == 0)
-	{
-		lineEditLanguage->setText(tr("English"));
-		indexLanguage = 1;
-	}
-	else if(utils_cfg_cmp_item(SystemCfg.language, "KOREAN") == 0)
-	{
+    if(utils_cfg_cmp_item(SystemCfg.language, "ENGLISH") == 0)
+    {
+        lineEditLanguage->setText(tr("English"));
+        indexLanguage = 1;
+    }
+    else if(utils_cfg_cmp_item(SystemCfg.language, "KOREAN") == 0)
+    {
         lineEditLanguage->setText(tr("Korean"));
-		indexLanguage = 2;
-	}
-	else if(utils_cfg_cmp_item(SystemCfg.language, "GERMAN") == 0)
-	{
-		lineEditLanguage->setText(tr("German"));
-		indexLanguage = 3;
-	}
-	else if(utils_cfg_cmp_item(SystemCfg.language, "SPANISH") == 0)
-	{
-		lineEditLanguage->setText(tr("Spanish"));
-		indexLanguage = 4;
-	}
-	else if(utils_cfg_cmp_item(SystemCfg.language, "FRENCH") == 0)
-	{
-		lineEditLanguage->setText(tr("French"));
-		indexLanguage = 5;
-	}
-	else if(utils_cfg_cmp_item(SystemCfg.language, "ITALIAN") == 0)
-	{
-		lineEditLanguage->setText(tr("Italian"));
-		indexLanguage = 6;
-	}
-	else if(utils_cfg_cmp_item(SystemCfg.language, "PORTUGUESE") == 0)
-	{
-		lineEditLanguage->setText(tr("Portuguese"));
-		indexLanguage = 7;
-	}
-	else if(utils_cfg_cmp_item(SystemCfg.language, "JAPANESE") == 0)
-	{
-		lineEditLanguage->setText(tr("Japanese"));
-		indexLanguage = 8;
-	}
+        indexLanguage = 2;
+    }
+    else if(utils_cfg_cmp_item(SystemCfg.language, "GERMAN") == 0)
+    {
+        lineEditLanguage->setText(tr("German"));
+        indexLanguage = 3;
+    }
+    else if(utils_cfg_cmp_item(SystemCfg.language, "SPANISH") == 0)
+    {
+        lineEditLanguage->setText(tr("Spanish"));
+        indexLanguage = 4;
+    }
+    else if(utils_cfg_cmp_item(SystemCfg.language, "FRENCH") == 0)
+    {
+        lineEditLanguage->setText(tr("French"));
+        indexLanguage = 5;
+    }
+    else if(utils_cfg_cmp_item(SystemCfg.language, "ITALIAN") == 0)
+    {
+        lineEditLanguage->setText(tr("Italian"));
+        indexLanguage = 6;
+    }
+    else if(utils_cfg_cmp_item(SystemCfg.language, "PORTUGUESE") == 0)
+    {
+        lineEditLanguage->setText(tr("Portuguese"));
+        indexLanguage = 7;
+    }
+    else if(utils_cfg_cmp_item(SystemCfg.language, "JAPANESE") == 0)
+    {
+        lineEditLanguage->setText(tr("Japanese"));
+        indexLanguage = 8;
+    }
 }
 void LanguageDialog::onButtonLanguageUp()
 {
 //    if(indexLanguage <= 1) { indexLanguage = 9; }
     if(indexLanguage <= 1) { indexLanguage = 5; }
 
-	switch(--indexLanguage)
-	{
-		case 1: { lineEditLanguage->setText(tr("English"   ));  utils_cfg_cpy_item(SystemCfg.language, "ENGLISH"   );  break; }
-		case 2: { lineEditLanguage->setText(tr("Korean"    ));  utils_cfg_cpy_item(SystemCfg.language, "KOREAN"    );  break; }
+    switch(--indexLanguage)
+    {
+        case 1: { lineEditLanguage->setText(tr("English"   ));  utils_cfg_cpy_item(SystemCfg.language, "ENGLISH"   );  break; }
+        case 2: { lineEditLanguage->setText(tr("Korean"    ));  utils_cfg_cpy_item(SystemCfg.language, "KOREAN"    );  break; }
         case 3: { lineEditLanguage->setText(tr("Spanish"   ));  utils_cfg_cpy_item(SystemCfg.language, "SPANISH"   );  break; }
         case 4: { lineEditLanguage->setText(tr("French"    ));  utils_cfg_cpy_item(SystemCfg.language, "FRENCH"    );  break; }
 #if 0    //yjsin [17/08/31] temporary use only english, korean
         case 3: { lineEditLanguage->setText(tr("German"    ));  utils_cfg_cpy_item(SystemCfg.language, "GERMAN"    );  break; }
-		case 4: { lineEditLanguage->setText(tr("Spanish"   ));  utils_cfg_cpy_item(SystemCfg.language, "SPANISH"   );  break; }
-		case 5: { lineEditLanguage->setText(tr("French"    ));  utils_cfg_cpy_item(SystemCfg.language, "FRENCH"    );  break; }
-		case 6: { lineEditLanguage->setText(tr("Italian"   ));  utils_cfg_cpy_item(SystemCfg.language, "ITALIAN"   );  break; }
-		case 7: { lineEditLanguage->setText(tr("Portuguese"));  utils_cfg_cpy_item(SystemCfg.language, "PORTUGUESE");  break; }
-		case 8: { lineEditLanguage->setText(tr("Japanese"  ));  utils_cfg_cpy_item(SystemCfg.language, "JAPANESE"  );  break; }
+        case 4: { lineEditLanguage->setText(tr("Spanish"   ));  utils_cfg_cpy_item(SystemCfg.language, "SPANISH"   );  break; }
+        case 5: { lineEditLanguage->setText(tr("French"    ));  utils_cfg_cpy_item(SystemCfg.language, "FRENCH"    );  break; }
+        case 6: { lineEditLanguage->setText(tr("Italian"   ));  utils_cfg_cpy_item(SystemCfg.language, "ITALIAN"   );  break; }
+        case 7: { lineEditLanguage->setText(tr("Portuguese"));  utils_cfg_cpy_item(SystemCfg.language, "PORTUGUESE");  break; }
+        case 8: { lineEditLanguage->setText(tr("Japanese"  ));  utils_cfg_cpy_item(SystemCfg.language, "JAPANESE"  );  break; }
 #endif
     }
     /*
@@ -146,72 +146,72 @@ void LanguageDialog::onButtonLanguageUp()
 void LanguageDialog::onButtonLanguageDown()
 {
     if(indexLanguage >= 4) { indexLanguage = 0; }
-//    if(indexLanguage >= 8) { indexLanguage = 0; }
-	switch(++indexLanguage)
-	{
-		case 1: { lineEditLanguage->setText(tr("English"   ));  utils_cfg_cpy_item(SystemCfg.language, "ENGLISH"   );  break; }
-		case 2: { lineEditLanguage->setText(tr("Korean"    ));  utils_cfg_cpy_item(SystemCfg.language, "KOREAN"    );  break; }
+    //if(indexLanguage >= 8) { indexLanguage = 0; }
+    switch(++indexLanguage)
+    {
+        case 1: { lineEditLanguage->setText(tr("English"   ));  utils_cfg_cpy_item(SystemCfg.language, "ENGLISH"   );  break; }
+        case 2: { lineEditLanguage->setText(tr("Korean"    ));  utils_cfg_cpy_item(SystemCfg.language, "KOREAN"    );  break; }
         case 3: { lineEditLanguage->setText(tr("Spanish"   ));  utils_cfg_cpy_item(SystemCfg.language, "SPANISH"   );  break; }
         case 4: { lineEditLanguage->setText(tr("French"    ));  utils_cfg_cpy_item(SystemCfg.language, "FRENCH"    );  break; }
 #if 0   //yjsin [17/08/31] temporary use only english, korean
         case 3: { lineEditLanguage->setText(tr("German"    ));  utils_cfg_cpy_item(SystemCfg.language, "GERMAN"    );  break; }
-		case 4: { lineEditLanguage->setText(tr("Spanish"   ));  utils_cfg_cpy_item(SystemCfg.language, "SPANISH"   );  break; }
-		case 5: { lineEditLanguage->setText(tr("French"    ));  utils_cfg_cpy_item(SystemCfg.language, "FRENCH"    );  break; }
-		case 6: { lineEditLanguage->setText(tr("Italian"   ));  utils_cfg_cpy_item(SystemCfg.language, "ITALIAN"   );  break; }
-		case 7: { lineEditLanguage->setText(tr("Portuguese"));  utils_cfg_cpy_item(SystemCfg.language, "PORTUGUESE");  break; }
-		case 8: { lineEditLanguage->setText(tr("Japanese"  ));  utils_cfg_cpy_item(SystemCfg.language, "JAPANESE"  );  break; }
+        case 4: { lineEditLanguage->setText(tr("Spanish"   ));  utils_cfg_cpy_item(SystemCfg.language, "SPANISH"   );  break; }
+        case 5: { lineEditLanguage->setText(tr("French"    ));  utils_cfg_cpy_item(SystemCfg.language, "FRENCH"    );  break; }
+        case 6: { lineEditLanguage->setText(tr("Italian"   ));  utils_cfg_cpy_item(SystemCfg.language, "ITALIAN"   );  break; }
+        case 7: { lineEditLanguage->setText(tr("Portuguese"));  utils_cfg_cpy_item(SystemCfg.language, "PORTUGUESE");  break; }
+        case 8: { lineEditLanguage->setText(tr("Japanese"  ));  utils_cfg_cpy_item(SystemCfg.language, "JAPANESE"  );  break; }
 #endif
     }
 }
 void LanguageDialog::keyPressEvent(QKeyEvent *event)
 {
-	switch(event->key())
-	{
-		case Qt::Key_Up:
-			{
-				     if(buttonLanguageUp->hasFocus())     { buttonClose->setFocus();            }
-				else if(buttonLanguageDown->hasFocus())   { buttonLanguageUp->setFocus();       }
-				else if(buttonClose->hasFocus())          { buttonLanguageDown->setFocus();     }
-				return;
-			}
-		case Qt::Key_Down:
-			{
-				     if(buttonLanguageUp->hasFocus())     { buttonLanguageDown->setFocus();     }
-				else if(buttonLanguageDown->hasFocus())   { buttonClose->setFocus();            }
-				else if(buttonClose->hasFocus())          { buttonLanguageUp->setFocus();       }
-				return;
-			}
-		case Qt::Key_Left:
-			{
-				     if(buttonSave->hasFocus())           { buttonClose->setFocus();            }
-				else if(buttonClose->hasFocus())          { buttonSave->setFocus();             }
-				return;
-			}
-		case Qt::Key_Right:
-			{
-				     if(buttonSave->hasFocus())           { buttonClose->setFocus();            }
-				else if(buttonClose->hasFocus())          { buttonSave->setFocus();             }
-				return;
-			}
-		case Qt::Key_Enter:
-			{
-				break;
-			}
-		case Qt::Key_Escape:
-			{
-				reject();
-				return;
-			}
-		default:
-			{
-				reject();
-				return;
-			}
-	}
+    switch(event->key())
+    {
+        case Qt::Key_Up:
+        {
+                 if(buttonLanguageUp   ->hasFocus())    { buttonClose        ->setFocus(); }
+            else if(buttonLanguageDown ->hasFocus())    { buttonLanguageUp   ->setFocus(); }
+            else if(buttonClose        ->hasFocus())    { buttonLanguageDown ->setFocus(); }
+            return;
+        }
+        case Qt::Key_Down:
+        {
+                 if(buttonLanguageUp   ->hasFocus())    { buttonLanguageDown ->setFocus(); }
+            else if(buttonLanguageDown ->hasFocus())    { buttonClose        ->setFocus(); }
+            else if(buttonClose        ->hasFocus())    { buttonLanguageUp   ->setFocus(); }
+            return;
+        }
+        case Qt::Key_Left:
+        {
+                 if(buttonSave         ->hasFocus())    { buttonClose        ->setFocus(); }
+            else if(buttonClose        ->hasFocus())    { buttonSave         ->setFocus(); }
+            return;
+        }
+        case Qt::Key_Right:
+        {
+                 if(buttonSave         ->hasFocus())    { buttonClose        ->setFocus(); }
+            else if(buttonClose        ->hasFocus())    { buttonSave         ->setFocus(); }
+            return;
+        }
+        case Qt::Key_Enter:
+        {
+            break;
+        }
+        case Qt::Key_Escape:
+        {
+            reject();
+            return;
+        }
+        default:
+        {
+            reject();
+            return;
+        }
+    }
 
-	QDialog::keyPressEvent(event);
+    QDialog::keyPressEvent(event);
 }
 void LanguageDialog::keyReleaseEvent(QKeyEvent *event)
 {
-	return;
+    return;
 }
