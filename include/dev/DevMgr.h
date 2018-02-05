fa195cc2352d9946f83c3c6b55f03bb38946415b
GyverJeong <skjeong@emastec.com>
2018-02-05 22:47:36 +0900

    Set GPS as follows - Get data of GPRMC and GNRMC - Reset and Cold Start - Receive GPRMC and GNRMC - Reset and Cold Start when reconnecting GPS terminal

--- include/dev/DevMgr.h
+++ include/dev/DevMgr.h
@@ -10,27 +10,28 @@ enum {
 	DEV_MAX,
 };
 
-int devmgr_dev_init                     (int maxSensor, int maxAlarm);
-int devmgr_dev_deinit                   (void);
-int devmgr_dev_deinit2                  (void);
-int devmgr_uart_open                    (int id, int baudrate, int databit, int stopbit, int parity);
-int devmgr_uart_close                   (int id);
-int devmgr_rs232_read                   (int id, unsigned char *data, int size);
-int devmgr_rs232_write                  (int id, unsigned char *data, int size);
-int devmgr_set_beep                     (int on);
-int devmgr_get_trigger                  (void);
-int devmgr_hi_wdg_start                 (void);
-int devmgr_hi_wdg_stop                  (void);
-int devmgr_hi_wdg_reboot                (void);
-int devmgr_hi_wdg_keep_alive            (void);
-int devmgr_hi_wdg_set_timeout           (int sec);
-int devmgr_hi_ir_init                   (void);
-int devmgr_hi_ir_deinit                 (void);
-int devmgr_hi_ir_read                   (unsigned long *datah, unsigned long *datal, unsigned long *state);
+int  devmgr_dev_init                    (int maxSensor, int maxAlarm);
+int  devmgr_dev_deinit                  (void);
+int  devmgr_dev_deinit2                 (void);
+int  devmgr_uart_open                   (int id, int baudrate, int databit, int stopbit, int parity);
+int  devmgr_uart_close                  (int id);
+int  devmgr_rs232_read                  (int id, unsigned char *data, int size);
+int  devmgr_rs232_write                 (int id, unsigned char *data, int size);
+int  devmgr_set_beep                    (int on);
+int  devmgr_get_trigger                 (void);
+int  devmgr_hi_wdg_start                (void);
+int  devmgr_hi_wdg_stop                 (void);
+int  devmgr_hi_wdg_reboot               (void);
+int  devmgr_hi_wdg_keep_alive           (void);
+int  devmgr_hi_wdg_set_timeout          (int sec);
+int  devmgr_hi_ir_init                  (void);
+int  devmgr_hi_ir_deinit                (void);
+int  devmgr_hi_ir_read                  (unsigned long *datah, unsigned long *datal, unsigned long *state);
 void devmgr_gpio_power_led_control      (int value);
 void devmgr_gpio_record_led_control     (int value);
 void devmgr_gpio_gsensor_led_control    (int value);
 void devmgr_gpio_network_led_control    (int value);
+void devmgr_gpio_gps_led_control        (int value);
 
 #ifdef __cplusplus
 }
